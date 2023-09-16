4ea5c3ecbd214d37d9ffadadd9c3a7e01f19db98
Pedro Garcia <pedroedu.gc02@gmail.com>
2023-09-16 17:01:42 -0300

    login com logo

--- frontend-react/src/components/Login/Login.tsx
+++ frontend-react/src/components/Login/Login.tsx
@@ -51,13 +51,19 @@ export default function Login() {
     }
 
     return (
-        <div>
+        <div>               
             <Grid container style={{ minHeight: '100vh'}}>
                 <Grid container item xs={12} sm={6} alignItems="center" direction="column" justifyContent="space-between" style={{padding: 10}}>
-                    <div />
+                    <div>
+                    <img 
+                        src="src/components/login/LOGOCRECHE.png" 
+                        style={{ width: '100%', height: '100%', objectFit: 'cover', position: 'relative', top: '30%'}} 
+                        alt="brand"
+                    />
+                    </div>
                         <div style={{ display: 'flex', flexDirection: 'column', maxWidth: 400, minWidth: 300}}>
                             <div>
-                                <h2 style={{color:"#000000", fontSize: 30, fontFamily:"Lucida Sans"}}>
+                                <h2 style={{color:"#000000", fontSize: 30, fontFamily:"Lucida Sans", textAlign: 'center'}}>
                                     Bem vindo!
                                 </h2>
                                 <h3 style={{color:"#000000", marginBottom:20, marginTop:5, fontFamily:"Verdana"}}>
@@ -70,7 +76,10 @@ export default function Login() {
                                 <Button color="primary" variant="contained">
                                     Entrar
                                 </Button>
-                                <p> Não possui um usuário ainda? <Button style={{ textTransform: 'none', outline: 'none' }}> Cadastre-se </Button></p>
+                                <p style={{color:"#000000"}}> Não possui um usuário ainda? 
+                                <Button style={{ textTransform: 'none', outline: 'none' }}> 
+                                    Cadastre-se 
+                                </Button></p>
                             </div>
                         </div>
                     <div />
