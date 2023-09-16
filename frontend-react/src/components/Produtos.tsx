685ac5d35e003b867f72f0e600f4b4cfc015440f
Leandro Vieira <leandradevieira38@gmail.com>
2023-09-16 17:25:08 -0300

    Tirando o menu do login

--- frontend-react/src/components/Produtos.tsx
+++ frontend-react/src/components/Produtos.tsx
@@ -1,11 +1,26 @@
+import { useLocation } from "react-router-dom";
+import Menu from "./Menu";
+
 export function Produto() {
-    return (
-        <div className="flex flex-col items-center justify-center ">
-            <div className="max-w-md mx-10 my-5 mb-4">
-                Página de produtos
-            </div>
-        </div>
-    )
+
+     // esta variável vai conter o username passado na navegação
+     const location = useLocation();
+     // recupera o username
+     //vamos utilizar Cookies para armazenar o username - BREVE
+     const username = location.state?.username || '';
+     
+       return (
+           <>
+           <div className="flex-col">
+           <Menu username={username}/>
+         </div>
+           <div className="flex flex-col items-center justify-center ">
+               <div className="max-w-md mx-10 my-5 mb-4">
+                   Página de Produtos
+               </div>
+           </div>
+           </>
+       )
 }
 
 export default Produto;
