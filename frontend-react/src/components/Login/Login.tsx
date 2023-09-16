cb7f1edd4f92518e2441d22b8515451f6be08ff2
Caio <caiotaveiradecastro@gmail.com>
2023-09-16 14:40:29 -0300

    Indentacao correta

--- frontend-react/src/components/Login/Login.tsx
+++ frontend-react/src/components/Login/Login.tsx
@@ -56,34 +56,27 @@ export default function Login() {
                 <Grid container item xs={12} sm={6} alignItems="center" direction="column" justifyContent="space-between" style={{padding: 10}}>
                     <div />
                         <div style={{ display: 'flex', flexDirection: 'column', maxWidth: 400, minWidth: 300}}>
-                        <div>
-                        <h2 style={{color:"#000000", fontSize: 30, fontFamily:"Lucida Sans"}}>
-                            Bem vindo!
-                        </h2>
-                        <h3 style={{color:"#000000", marginBottom:20, marginTop:5, fontFamily:"Verdana"}}>
-                            Digite suas credenciais para acessar o site.
-                        </h3>
-                    </div>
-                            <Grid container alignItems="center" direction="column">
-                                <img
-                                    src="padlock.png" 
-                                    width={'8%'} 
-                                    alt="logo"
-                                />
-                            </Grid>
+                            <div>
+                                <h2 style={{color:"#000000", fontSize: 30, fontFamily:"Lucida Sans"}}>
+                                    Bem vindo!
+                                </h2>
+                                <h3 style={{color:"#000000", marginBottom:20, marginTop:5, fontFamily:"Verdana"}}>
+                                    Digite suas credenciais para acessar o site.
+                                </h3>
+                            </div>
                             <TextField label="Login" margin="normal" />
                             <TextField label="Senha" margin="normal" type="password" />
                             <div style={{ display: 'flex', flexDirection: 'column', maxWidth: 400, minWidth: 300, marginTop: 15, boxShadow: '0px 2px 9px #6c63ff'}}>
-                            <Button>
-                                Entrar
-                            </Button>
-                        </div>
+                                <Button>
+                                    Entrar
+                                </Button>
+                            </div>
                         </div>
                     <div />
                 </Grid>
                 <Grid item xs={12} sm={6}>
                     <img 
-                        src="login.png" 
+                        src="src/components/login/login.png" 
                         style={{ width: '100%', height: '100%', objectFit: 'cover'}} 
                         alt="brand"
                     />
