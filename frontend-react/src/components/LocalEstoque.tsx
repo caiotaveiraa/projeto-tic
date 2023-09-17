c9425f457f1016719e90c9014c877a0a6d94f077
Pedro Garcia <pedroedu.gc02@gmail.com>
2023-09-17 12:12:19 -0300

    Local Esgtoqeu com MOdal

--- frontend-react/src/components/LocalEstoque.tsx
+++ frontend-react/src/components/LocalEstoque.tsx
@@ -1,8 +1,10 @@
 
 import {useLocation} from 'react-router-dom';
 import {useState, useEffect} from 'react'
-import {MdEdit, MdOutlineDeleteOutline} from 'react-icons/md'
+import {MdOutlineEdit, MdOutlineDeleteOutline, } from 'react-icons/md'
+import {HiOutlinePlusCircle} from 'react-icons/hi'
 import Menu from './Menu';
+
 
 interface localEstoqueProps { // tipo de dado
   idlocal: number,
@@ -21,8 +23,9 @@ export default function LocalEstoque()  {
   const [locais, setLocais] = useState<localEstoqueProps[]>([])
 
   // variáveis de estado para os campos do formulário
-  const [nomelocal, setName] = useState('')
-  
+  const [showModal, setShowModal] = useState(false);
+  const [nomelocal, setNomeLocal] = useState('');
+
   // diferencia se vai inserir (id = 0) ou editar (id não for 0) um produto
   const [idlocal, setId] = useState(0)
 
@@ -46,29 +49,105 @@ export default function LocalEstoque()  {
     buscaLocais()
   } , [username])
 
+    // função para remover um local de estoque pela tela
+    const handleRemove = async (id: number) => {
+      let confirma = confirm('Deseja remover esse local de estoque?')
+      if (confirma) {
+        // requisição DELETE para remover um produto através da API
+        await fetch(`http://localhost:3333/locaisEstoque/delete/${id}`, {
+          method: 'DELETE'
+        })
+        .then( response => {
+          return response.json()
+        })
+        .catch(error => {
+            alert(error)
+        })
+        // atualiza a lista dos locais - removendo o local deletado
+        // SetLocais vai receber como parâmetro a lista de locais atual após delete
+        // retirando o local que foi removido
+        setLocais(locais.filter( (Local) => Local.idlocal !== id ))
+      }
+    }
+
+    const handleEdit = (Local: localEstoqueProps) => {
+      setName(Local.nomelocal)
+      console.log(Local.idlocal)
+      setId(Local.idlocal) // usado para eu filtrar se é uma criação ou edição do local
+    }
+
+    const handleOpenModal = () => {
+      setShowModal(true);
+    }
+  
+    const handleCloseModal = () => {
+      setShowModal(false);
+    }
+
+    const handleFormSubmit = async (e) => {
+      e.preventDefault();
+  
+      // Envie os dados do formulário para a API ou faça o que for necessário para cadastrar o estoque.
+  
+      // Após o cadastro bem-sucedido, feche a modal.
+      setShowModal(false);
+    }
+
   return (
     <>
-    
     <div className="flex-col">
         <Menu username={username}/>
     </div>
-    <div className="flex flex-col items-center justify-center ">
-      <div className="max-w-md mx-10 my-5 mb-4">
+    <div className="w-full justify-center">
+      <div className="max-w-lg mx-10 my-5 mb-4 "> 
         {/* lista de Locais dentro de uma tabela */}
-        <h2 className="font-bold mb-4"> Lista de Locais </h2>
-        <table className="w-full border border-gray-300">
+        <div style={{ background: "linear-gradient(to right, #40E0D0, #4682B4)"}}>
+          <h2 className="font-bold mb-4" style={{ color: '#000000'}}> Locais de Locais </h2>
+        </div>
+        <button onClick={() => handleOpenModal()} style={{backgroundColor: '#A4EA4F', color: '#000000',  display: 'flex', alignItems: 'center', height: '25px', marginBottom: '10px'}} > 
+          <HiOutlinePlusCircle size={15} style={{ color: '#000000', marginRight: '3px' }}/> <span style={{ fontSize: '10px'}}> Novo registro  </span>
+        </button>
+        {/* Modal para cadastrar estoque */}
+        {showModal && (
+          <div className="modal" 	
+          style={{position: 'fixed', top: '0', left: '0', right: '0', bottom: '0', background: 'rgba(0, 0, 0, 0.7)', zIndex: '1000', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+            <div className="modal-content"  style={{background: '#fff', padding: '20px', width: '80%', maxWidth: '400px', borderRadius: '5px', boxShadow: '0 2px 4px rgba(0, 0, 0, 0.2)'}}>
+              <span className="close" onClick={handleCloseModal} style={{ color: '#000000'}} >&times;</span> 
+              <span style={{ color: '#000000'}}> Cadastro de Estoque </span> 
+              <form onSubmit={handleFormSubmit}>
+                <label htmlFor="nomeLocal" style={{ color: '#000000'}}> Nome do Local: </label>
+                <input type="text" id="nomeLocal" name="nomeLocal" value={nomelocal} onChange={(e) => setNomeLocal(e.target.value)} required />
+                <button type="submit"  style={{backgroundColor: '#A4EA4F', color: '#000000',  display: 'flex', alignItems: 'center', height: '25px', marginBottom: '10px'}}>
+                  Cadastrar
+                </button>
+              </form>
+            </div>
+          </div>
+        )}
+
+        <table className="w-full">
           <thead>
-            <tr className="bg-gray-200">
-              <th className="border border-gray-300 px-4 py-2">ID</th>
-              <th className="border border-gray-300 px-4 py-2">Nome</th>
+            <tr className="bg-transparent">
+              <th className=" px-4 py-2 text-black">Código</th>
+              <th className=" px- py-2  text-black" >Nome</th>
             </tr>
           </thead>
           <tbody>
             {
               locais.map( (Local) => (
-                <tr key={Local.id}>
-                  <td className="border border-gray-300 px-4 py-2"  >{Local.idlocal}</td>
-                  <td className="border border-gray-300 px-4 py-2">{Local.nomelocal}</td>
+                <tr key={Local.idlocal}>
+                  <td className="border border-gray-300 px-4 py-2 text-black "  >{Local.idlocal}</td>
+                  <td className="border border-gray-300 px-4 py-2 text-black "  >{Local.nomelocal}</td>
+                  <td className="border border-gray-300">
+                      <button onClick={() => handleEdit(Local)} style={{backgroundColor: 'transparent'}} > 
+                        <MdOutlineEdit size={20} style={{ color: '#1E90FF'}}/>
+                      </button>
+                    </td>
+                    <td className="border border-gray-300">
+                      <button onClick={() => handleRemove(Local.idlocal)} style={{backgroundColor: 'transparent'}}> 
+                        <MdOutlineDeleteOutline size={20} style={{ color: 'red' }}  />
+                      </button>
+                    </td> 
                 </tr>
               ) /* fim da função dentro do map */
               ) /* fim do map */
